41c01141fa94a0c3ce7db9676f53d5abae8c40ba
Dac Phuong <phuonghdth2009010@gmail.com>
2021-08-04 10:09:19 +0700

    edit heroku

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-//Route::get('',[ProductController::class,'index'])->name('listProduct');
+Route::get('',[ProductController::class,'index'])->name('listProduct');
 Route::get('/create',[ProductController::class,'create']);
 Route::post('/create',[ProductController::class,'store']);
 
